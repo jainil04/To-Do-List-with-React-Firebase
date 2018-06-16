780cb63a6bc3642cab6f6554175ace61211e980b
jainil04 <jainil04@gmail.com>
2018-06-16 03:02:20 -0700

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAS3lVwySSGOriBAIWQsESOkBNXnrZQ-ns",
+    authDomain: "react-notes-39620.firebaseapp.com",
+    databaseURL: "https://react-notes-39620.firebaseio.com",
+    projectId: "react-notes-39620",
+    storageBucket: "react-notes-39620.appspot.com",
+    messagingSenderId: "315269028870"
+  };
